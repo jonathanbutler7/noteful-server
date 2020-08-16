17cab36ad93aa529077642d70a2e1d64f168feb9
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-08-15 20:07:37 -0700

    CRUD requests for /folder now exist in folder-router

--- src/app.js
+++ src/app.js
@@ -4,13 +4,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const NoteService = require("./note/note-service");
-const FolderService = require("./folder/folder-service");
 const noteRouter = require("./note/note-router");
 const folderRouter = require("./folder/folder-router");
-const path = require("path");
 
-const jsonParser = express.json();
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -20,74 +16,6 @@ app.use(helmet());
 app.use(cors());
 app.use("/api/notes", noteRouter);
 app.use('/api/folders', folderRouter)
-
-
-
-app.get("/folders", (req, res, next) => {
-  FolderService.getAllFolders(req.app.get("db"))
-    .then((folders) => {
-      res.json(folders);
-    })
-    .catch(next);
-});
-
-app.delete("/folders/:folder_id", jsonParser, (req, res, next) => {
-  const id = req.params.folder_id;
-
-  FolderService.deleteFolder(req.app.get("db"), id).then(() => {
-    res.status(204).end();
-  });
-});
-
-app.patch("/folders/:folder_id", jsonParser, (req, res, next) => {
-  const id = req.params.folder_id;
-  const { folder_name } = req.body;
-  const folderToUpdate = { folder_name };
-
-  const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length;
-
-  if (numberOfValues === 0) {
-    return res.status(400).json({
-      error: { message: `Request body must contain folder_name` },
-    });
-  }
-  FolderService.updateFolder(req.app.get("db"), id, folderToUpdate)
-    .then((numRowsAffected) => {
-      res.status(204).end();
-    })
-    .catch(next);
-});
-
-app.get("/folders/:folder_id", jsonParser, (req, res, next) => {
-  const id = req.params.folder_id;
-
-  FolderService.getById(req.app.get("db"), id)
-    .then((folder) => {
-      if (!folder) {
-        res.status(404).json({
-          error: { message: `Folder with id ${id} does not exist.` },
-        });
-      }
-      res.status(201).json(folder);
-    })
-    .catch(next);
-});
-
-app.post("/folders", jsonParser, (req, res, next) => {
-  const { folder_name } = req.body;
-  const newFolder = { folder_name };
-
-  if (!folder_name) {
-    return res.status(400).json({
-      error: { message: `Please enter a field for folder_name` },
-    });
-  }
-  FolderService.insertFolder(req.app.get("db"), newFolder)
-    .then((folder) => {
-      res.status(201).json(newFolder);
-    })
-    .catch(next);
-});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
--- src/folder/folder-router.js
+++ src/folder/folder-router.js
@@ -1,12 +1,74 @@
-const express = require('express')
+const express = require("express");
+const FolderService = require("./folder-service");
 
-const folderRouter = express.Router()
-const jsonParser = express.json()
+const folderRouter = express.Router();
+const jsonParser = express.json();
 
 folderRouter
-    .route('/')
-    .get((req, res, next) => {
-        res.json("what up")
-    })
+  .route("/")
+  .get((req, res, next) => {
+    FolderService.getAllFolders(req.app.get("db"))
+      .then((folders) => {
+        res.json(folders);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const newFolder = { folder_name };
 
-module.exports = folderRouter
+    if (!folder_name) {
+      return res.status(400).json({
+        error: { message: `Please enter a field for folder_name` },
+      });
+    }
+    FolderService.insertFolder(req.app.get("db"), newFolder)
+      .then((folder) => {
+        res.status(201).json(newFolder);
+      })
+      .catch(next);
+  });
+
+folderRouter
+    .route("/:folder_id")
+    .get(jsonParser, (req, res, next) => {
+    const id = req.params.folder_id;
+
+    FolderService.getById(req.app.get("db"), id)
+        .then((folder) => {
+        if (!folder) {
+            res.status(404).json({
+            error: { message: `Folder with id ${id} does not exist.` },
+            });
+        }
+        res.status(201).json(folder);
+        })
+        .catch(next);
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const id = req.params.folder_id;
+        const { folder_name } = req.body;
+        const folderToUpdate = { folder_name };
+      
+        const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length;
+      
+        if (numberOfValues === 0) {
+          return res.status(400).json({
+            error: { message: `Request body must contain folder_name` },
+          });
+        }
+        FolderService.updateFolder(req.app.get("db"), id, folderToUpdate)
+          .then((numRowsAffected) => {
+            res.status(204).end();
+          })
+          .catch(next);
+      })
+      .delete(jsonParser, (req, res, next) => {
+        const id = req.params.folder_id;
+      
+        FolderService.deleteFolder(req.app.get("db"), id).then(() => {
+          res.status(204).end();
+        });
+      })
+
+module.exports = folderRouter;
