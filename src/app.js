47e5d783bffa6d95847913f474b24530f9fb7cee
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-08-15 18:12:15 -0700

    commented out line 20

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use('/api/note', noteRouter)
+// app.use('/api/note', noteRouter)
 // app.use('/api/folder', folderRouter)
 
 app.get("/notes", (req, res, next) => {
